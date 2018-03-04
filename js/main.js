52ea2637a4754133ca871408e7f6ad1c1f0c9343
JesseStyle <35957342+JesseStyle@users.noreply.github.com>
2018-03-04 21:35:01 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {  
-        //为超链接加上target='_blank'属性  
-        $('a[href^="http"]').each(function() {  
-            $(this).attr('target', '_blank');  
-        });  
-    });  
-
 $(document).ready(function() {
 
   $('a.blog-button').click(function() {
